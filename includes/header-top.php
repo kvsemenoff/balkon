e0050ab1209de99e7bf0b4848e3059a7c191f8d2
Berzeg07 <berezg86@gmail.com>
2017-03-15 13:17:04 +0300

    29.14

--- includes/header-top.php
+++ includes/header-top.php
@@ -1,8 +1,15 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<section class="section section_padding dd-header-top">
+	<div class=" container">
+		<div class="row">
+			<div class="col-md-3">
+
+			</div>
+			<div class="col-md-6">
+
+			</div>
+			<div class="col-md-3">
+
+			</div>
 		</div>
 	</div>
-</div>
 </section>
