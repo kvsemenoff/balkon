320d3a0181373f55bc322fdc1c8d79c633c0cc08
roshett <roshette@yandex.ru>
2017-03-19 01:58:25 +0300

    cold-start

--- cold.php
+++ cold.php
@@ -1,7 +1,38 @@
 <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/header-top.php'); ?>
-
+<?php require_once('bread.php'); ?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-5">
+		 	<div class="wrap-cold-h1">
+		 		<h1>холодное остекление</h1>
+		 	</div>
+			<div class="cold-pic">
+			</div>
+		</div>
+		<div class="col-md-1">
+			<div class="cold-circle"></div>
+		</div>
+		<div class="col-md-6">
+			<div class="cold-wrap-text">
+				<span class="r-text">
+				При холодном остеклении балкона разница температуры с улицей - 5-10 градусов. Конструкция сочетает в себе несколько глухих и раздвижных секций, открытие которых не требует дополнительного пространства. <br>
+				При таком варианте остекления используется алюминиевый профиль, в который можно установить одинарное стекло или стеклопакет толщиной не более 16 миллиметров.<br>
+				Алюминиевый профиль узкий, благодаря чему увеличивается световой проем, что придает спространству помещения особую легкость и воздушность.
+			</span><br><br>
+				<span class="b-text">
+						Узнайте больше о холодном остеклении по номеру:
+				</span>
+			</div>
+			<div class="cold-phone">
+				<div class="cold-phone-icon"></div>
+				<a href="tel:+7351555-32-22">
+					<span class="cold-number">+7 (351) <strong>555-32-22</strong></span>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
 <?php require_once('includes/footer.php'); ?>
-
 </body>
 </html>
