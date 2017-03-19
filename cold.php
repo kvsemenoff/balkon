dea4f6dbb353089019bcd7b96b8f8cae1b9f116e
roshett <roshette@yandex.ru>
2017-03-19 11:14:16 +0300

    cold-start

--- cold.php
+++ cold.php
@@ -3,35 +3,89 @@
 <?php require_once('bread.php'); ?>
 <div class="container">
 	<div class="row">
-		<div class="col-md-5">
-		 	<div class="wrap-cold-h1">
-		 		<h1>холодное остекление</h1>
-		 	</div>
-			<div class="cold-pic">
+		<div class="wrap-first-cold">
+			<div class="col-md-5">
+				<div class="wrap-cold-h1">
+					<h1>холодное остекление</h1>
+				</div>
+				<div class="cold-pic">
+				</div>
 			</div>
-		</div>
-		<div class="col-md-1">
-			<div class="cold-circle"></div>
-		</div>
-		<div class="col-md-6">
-			<div class="cold-wrap-text">
-				<span class="r-text">
-				При холодном остеклении балкона разница температуры с улицей - 5-10 градусов. Конструкция сочетает в себе несколько глухих и раздвижных секций, открытие которых не требует дополнительного пространства. <br>
-				При таком варианте остекления используется алюминиевый профиль, в который можно установить одинарное стекло или стеклопакет толщиной не более 16 миллиметров.<br>
-				Алюминиевый профиль узкий, благодаря чему увеличивается световой проем, что придает спространству помещения особую легкость и воздушность.
-			</span><br><br>
-				<span class="b-text">
+			<div class="col-md-1">
+				<div class="cold-circle"></div>
+			</div>
+			<div class="col-md-6">
+				<div class="cold-wrap-text">
+					<span class="r-text">
+						При холодном остеклении балкона разница температуры с улицей - 5-10 градусов. Конструкция сочетает в себе несколько глухих и раздвижных секций, открытие которых не требует дополнительного пространства. <br>
+						При таком варианте остекления используется алюминиевый профиль, в который можно установить одинарное стекло или стеклопакет толщиной не более 16 миллиметров.<br>
+						Алюминиевый профиль узкий, благодаря чему увеличивается световой проем, что придает спространству помещения особую легкость и воздушность.
+					</span><br><br>
+					<span class="b-text">
 						Узнайте больше о холодном остеклении по номеру:
-				</span>
-			</div>
-			<div class="cold-phone">
-				<div class="cold-phone-icon"></div>
-				<a href="tel:+7351555-32-22">
-					<span class="cold-number">+7 (351) <strong>555-32-22</strong></span>
-				</a>
+					</span>
+				</div>
+				<div class="cold-phone">
+					<div class="cold-phone-icon"></div>
+					<a href="tel:+7351555-32-22">
+						<span class="cold-number">+7 (351) <strong>555-32-22</strong></span>
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
+</div>
+<div class="second-cold">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="db-our-h1">преимущества холодного остекления</h1>
+				<div class="cold-p">
+					<p class="select-promo">Позволит превратить балкон в хозяйственное, техническое или подсобное помещение и будет препятствовать попаданию пыли и грызи, а также снижать уровень шума и количество запахов.</p>
+				</div>
+			</div>
+		</div>
+	</div><div class="container">
+		<div class="row">
+			<div class="cold-some-text">
+				<div class="col-md-3">
+				<div class="cold-block1">
+					<div class="cold-wrap-img">
+					</div>
+					<span>Низкий вес</span>
+					<p>	Благодаря низкому весу можно остеклять балкон в любом техническом состоянии без какой-либо опасности.</p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="cold-block2">
+					<div class="cold-wrap-img">
+					</div>
+					<span>Раздвижные створки</span>
+					<p>	Раздвижные створки позволяют значительно экномить место при открывании окон. Количество створок не ограничено, что делает удобным открытие любой части балкона.</p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="cold-block3">
+					<div class="cold-wrap-img">
+					</div>
+					<span>Больше света</span>
+					<p>	Узкий профиль не занимает много места, позволяя максимуму света поступать в помещение.</p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="cold-block4">
+					<div class="cold-wrap-img">
+					</div>
+					<span>Низкая цена</span>
+					<p>	Благодаря низкой стоимости этот вариант остекления доступен каждому.</p>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="cold-our-works">
+	<?php require_once('includes/our.php'); ?>
 </div>
 <?php require_once('includes/footer.php'); ?>
 </body>
