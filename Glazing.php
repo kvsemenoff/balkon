0ba698e1bcc71cf9d337b3cfab619b86a28aaf58
Berzeg07 <berezg86@gmail.com>
2017-03-20 17:18:36 +0300

    29.14

--- Glazing.php
+++ Glazing.php
@@ -2,6 +2,7 @@
 <?php require_once('includes/header-top.php'); ?>
 
 
+<!-- Выберите вид отсекленения балконов -->
 
 <section>
 
@@ -15,54 +16,162 @@
 			</div>
 
 			<div class="col-md-6">
-
-				<div class="dd-title_vid">
-					<div class="dd-title_view">
-						<span>Холодное остекленение</span>
+				<div class="dd-box-wrap">
+					<div class="dd-title_vid">
+						<div class="dd-title_view">
+							<span>Холодное остекленение</span>
+						</div>
 					</div>
-				</div>
 
-				<div class="dd-img_vid">
-					<img src="img/b1.png" alt="">
-				</div>
-
-				<div class="dd-right_vid">
-					<div class="dd-txt_vid">
-						<span>Выгодная стоимость</span><br>
-						<span>Удобно для небольшого<br> балкона</span><br>
-						<span>Долгий срок службы</span><br>
-						<span>от<b> 12860 </b>руб.</span>
+					<div class="dd-img_vid float_vid">
+						<img src="img/b1.png" alt="">
 					</div>
-					<div class="dd-but_txt">
-						<a class="button-1" href="#">Подробнее</a>
-					</div>
-				</div>
 
+					<div class="dd-right_vid float_vid">
+						<div class="dd-txt_vid">
+							<span>Выгодная стоимость</span><br>
+							<span>Удобно для небольшого<br> балкона</span><br>
+							<span class="mb_none_vid">Долгий срок службы</span><br>
+							<span class="dd-sp4_vid">от<b> 12860 </b>руб.</span>
+						</div>
+						<div class="dd-but_txt">
+							<a class="button-my" href="#">Подробнее</a>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
 			</div>
 
 			<div class="col-md-6">
-				
-			</div>
-			<div class="clearfix"></div>
+				<div class="dd-box-wrap">
+					<div class="dd-title_vid mt2">
+						<div class="dd-title_view">
+							<span>Теплое остекленение</span>
+						</div>
+					</div>
 
-			<div class="col-md-6">
-				
-			</div>
-			<div class="col-md-6">
-				
-			</div>
+					<div class="dd-img_vid float_vid">
+						<img src="img/b2.png" alt="">
+					</div>
 
-			<div class="clearfix"></div>
+					<div class="dd-right_vid float_vid">
+						<div class="dd-txt_vid">
+							<span>На балконе всегда тепло</span><br>
+							<span>Самая лучшая защита от шума</span><br>
+							<span class="mb_none_vid">Можно объеденить балкон с<br> 
+								комнатой</span><br>
+								<span class="dd-sp4_vid">от<b> 21400 </b>руб.</span>
+							</div>
+							<div class="dd-but_txt">
+								<a class="button-my" href="#">Подробнее</a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+
+				<div class="col-md-6">
+					<div class="dd-box-wrap">
+						<div class="dd-title_vid mt">
+							<div class="dd-title_view">
+								<span>Остекленение с выносом</span>
+							</div>
+						</div>
+
+						<div class="dd-img_vid float_vid">
+							<img src="img/b3.png" alt="">
+						</div>
+
+						<div class="dd-right_vid float_vid">
+							<div class="dd-txt_vid ">
+								<span>Увеличение площади на <br> балконе</span><br>
+								<span class="mb">Создание удобных<br> подоконников</span><br>
+								<span class="dd-sp4_vid">от<b> 12860 </b>руб.</span>
+							</div>
+							<div class="dd-but_txt">
+								<a class="button-my" href="#">Подробнее</a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="dd-box-wrap">
+						<div class="dd-title_vid mt">
+							<div class="dd-title_view">
+								<span>Панорамное остекленение</span>
+							</div>
+						</div>
+
+						<div class="dd-img_vid float_vid">
+							<img src="img/b4.png" alt="">
+						</div>
+
+						<div class="dd-right_vid float_vid">
+							<div class="dd-txt_vid">
+								<span>Легкая конструкция</span><br>
+								<span>Много света и тепла в вашем<br> доме</span><br>
+								<span class="mb_none_vid">Эстетическая красота на<br> высшем уровне</span><br>
+								<span class="dd-sp4_vid">от<b> 21430 </b>руб.</span>
+							</div>
+							<div class="dd-but_txt">
+								<a class="button-my" href="#">Подробнее</a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+
+				<div class="clearfix"></div>
+				<div class="col-md-12">
+					<div class="dd-bottom-txt_vid">
+						<p>
+							Кто-то желает превратить балкон в теплую и комфортабельную комнату, кто-то использует его в качестве технического или хозяйственного помещения, а кому-то достаточно, чтобы внутрь лоджии просто не попадали осадки, пыль и грязь.
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
 
-</section>
+	</section>
 
-
-
+	<!-- Конец выберите вид отсекленения балконов -->
 
 
-<?php require_once('includes/footer.php'); ?>
+	<!-- Без доп платежей -->
+
+	<section class="dd-wrap_bp">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="dd-content_pb">
+						<div class="dd-kr_pb">
+							<img src="img/kr3.png" alt="">
+						</div>
+						<div class="dd-title_bp">
+							<h1>У нас без дополнительных платежей!</h1>
+							<span>Озвученные при расчете суммы закрепляются в договоре без последующих изменений
+							</span>
+						</div>
+						<div class="dd-img_bp">
+							<img src="img/log.png" alt="">
+						</div>
+						<div class="dd-slogan_bp">
+							<span>Еще мы предоставляем бесплатную гарантию на свои работы</span><br>
+							<a class="button-my" href="#">Подробнее о гарантии</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Конец Без доп платежей -->
+
+
+
+	<?php require_once('includes/footer.php'); ?>
 
 </body>
 </html>
