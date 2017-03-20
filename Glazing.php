0636a98df1f26a3d162148751eeedbc394f26771
Berzeg07 <berezg86@gmail.com>
2017-03-20 14:20:54 +0300

    29.14

--- Glazing.php
+++ Glazing.php
@@ -1,6 +1,67 @@
 <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/header-top.php'); ?>
 
+
+
+<section>
+
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12">
+				<div class="dd-main-title_vid">
+					<h1>Выберите вид остекленения балконов</h1>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+
+				<div class="dd-title_vid">
+					<div class="dd-title_view">
+						<span>Холодное остекленение</span>
+					</div>
+				</div>
+
+				<div class="dd-img_vid">
+					<img src="img/b1.png" alt="">
+				</div>
+
+				<div class="dd-right_vid">
+					<div class="dd-txt_vid">
+						<span>Выгодная стоимость</span><br>
+						<span>Удобно для небольшого<br> балкона</span><br>
+						<span>Долгий срок службы</span><br>
+						<span>от<b> 12860 </b>руб.</span>
+					</div>
+					<div class="dd-but_txt">
+						<a class="button-1" href="#">Подробнее</a>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="col-md-6">
+				
+			</div>
+			<div class="clearfix"></div>
+
+			<div class="col-md-6">
+				
+			</div>
+			<div class="col-md-6">
+				
+			</div>
+
+			<div class="clearfix"></div>
+		</div>
+	</div>
+
+</section>
+
+
+
+
+
 <?php require_once('includes/footer.php'); ?>
 
 </body>
