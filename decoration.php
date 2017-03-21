fef0205f5d763a54abfd440d89404d6ab7c026b0
engine77 <zaur.1kager@gmail.com>
2017-03-21 13:34:49 +0300

    decor

--- decoration.php
+++ decoration.php
@@ -1,7 +1,385 @@
 <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/header-top.php'); ?>
+	
+	<section class="section section_padding bgcolor">
+
+		<div class="container">
+			<h2 class="dfdectopcap">Выберите материал отделки</h2>
+
+			<div class="dfdecorblock">
+
+				<div class="dfdecorbox">
+					<div class="dftopcaption">
+						<h4 class="dfcapdecor">Отделка ламинированными панелями</h4>
+						<span class="dfcapprice">от
+							<strong> 1400 
+							</strong>
+							руб.		
+						</span>
+						<div class="clear"></div>
+					</div>
+					<div class="dfdecorhoverbox">
+						<div class="dfdecorimg">
+							<img src="img/decor1.jpg" alt="">
+						</div>
+						<div class="dfhovbox">
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<a href="#" class="dflinkdecor">выбрать</a>
+						</div>
+					</div>
+				</div>
+				<div class="dfdecorbox mr0">
+					<div class="dftopcaption">
+						<h4 class="dfcapdecor">Отделка пластиковыми панелями</h4>
+						<span class="dfcapprice">от
+							<strong> 1400 
+							</strong>
+							руб.		
+						</span>
+						<div class="clear"></div>
+					</div>
+					<div class="dfdecorhoverbox">
+						<div class="dfdecorimg">
+							<img src="img/decor2.jpg" alt="">
+						</div>
+						<div class="dfhovbox">
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<a href="#" class="dflinkdecor">выбрать</a>
+						</div>
+					</div>
+				</div>
+				<div class="dfdecorbox">
+					<div class="dftopcaption">
+						<h4 class="dfcapdecor">Отделка вагонкой</h4>
+						<span class="dfcapprice">от
+							<strong> 1400 
+							</strong>
+							руб.		
+						</span>
+						<div class="clear"></div>
+					</div>
+					<div class="dfdecorhoverbox">
+						<div class="dfdecorimg">
+							<img src="img/decor3.jpg" alt="">
+						</div>
+						<div class="dfhovbox">
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<a href="#" class="dflinkdecor">выбрать</a>
+						</div>
+					</div>
+				</div>
+				<div class="dfdecorbox mr0">
+					<div class="dftopcaption">
+						<h4 class="dfcapdecor">Отделка МДФ</h4>
+						<span class="dfcapprice">от
+							<strong> 1400 
+							</strong>
+							руб.		
+						</span>
+						<div class="clear"></div>
+					</div>
+					<div class="dfdecorhoverbox">
+						<div class="dfdecorimg">
+							<img src="img/decor4.jpg" alt="">
+						</div>
+						<div class="dfhovbox">
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<a href="#" class="dflinkdecor">выбрать</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="dfdecorbox">
+					<div class="dftopcaption">
+						<h4 class="dfcapdecor">Отделка камнем</h4>
+						<span class="dfcapprice">от
+							<strong> 1400 
+							</strong>
+							руб.		
+						</span>
+						<div class="clear"></div>
+					</div>
+					<div class="dfdecorhoverbox">
+						<div class="dfdecorimg">
+							<img src="img/decor5.jpg" alt="">
+						</div>
+						<div class="dfhovbox">
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<a href="#" class="dflinkdecor">выбрать</a>
+						</div>
+					</div>
+				</div>
+				<div class="dfdecorbox mr0">
+					<div class="dftopcaption">
+						<h4 class="dfcapdecor">Отделка кирпичом</h4>
+						<span class="dfcapprice">от
+							<strong> 1400 
+							</strong>
+							руб.		
+						</span>
+						<div class="clear"></div>
+					</div>
+					<div class="dfdecorhoverbox">
+						<div class="dfdecorimg">
+							<img src="img/decor6.jpg" alt="">
+						</div>
+						<div class="dfhovbox">
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<a href="#" class="dflinkdecor">выбрать</a>
+						</div>
+					</div>
+				</div>
+				<div class="dfdecorbox">
+					<div class="dftopcaption">
+						<h4 class="dfcapdecor">Отделка штукатуркой</h4>
+						<span class="dfcapprice">от
+							<strong> 1400 
+							</strong>
+							руб.		
+						</span>
+						<div class="clear"></div>
+					</div>
+					<div class="dfdecorhoverbox">
+						<div class="dfdecorimg">
+							<img src="img/decor7.jpg" alt="">
+						</div>
+						<div class="dfhovbox">
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<a href="#" class="dflinkdecor">выбрать</a>
+						</div>
+					</div>
+				</div>
+				<div class="dfdecorbox mr0">
+					<div class="dftopcaption">
+						<h4 class="dfcapdecor">Утепление</h4>
+						<span class="dfcapprice">от
+							<strong> 1400 
+							</strong>
+							руб.		
+						</span>
+						<div class="clear"></div>
+					</div>
+					<div class="dfdecorhoverbox">
+						<div class="dfdecorimg">
+							<img src="img/decor8.jpg" alt="">
+						</div>
+						<div class="dfhovbox">
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<a href="#" class="dflinkdecor">выбрать</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="dftxtbottom">Кто-то желает превратить балкон в теплую и комфортабельную комнату, кто-то использует его в качестве технического или хозяйственного помещения, а кому-то достаточно, чтобы внутрь лоджии просто не попадали осадки, пыль и грязь.</div>
+		</div>
+	</section>
+
+
+	<section class="section dfsection_padding bgcolor">
+
+		<div class="container">
+			<h2 class="dfdectopcap " id="margin0">примеры наших работ</h2>
+			
+			<div class="dfboxcarousel dfnav1">
+				<h4 class="dfcarouselcap"><span>Отделка балкона</span></h4>
+				<div class="dfcarousel1">
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel1.jpg" alt="">
+						</div>
+					</div>
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel2.jpg" alt="">
+						</div>
+					</div>
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel3.jpg" alt="">
+						</div>
+					</div>
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel4.jpg" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="dfboxcarousel dfnav2">
+				<h4 class="dfcarouselcap"><span>Установка шкафов</span></h4>
+				<div class="dfcarousel2">
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel5.jpg" alt="">
+						</div>
+					</div>
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel6.jpg" alt="">
+						</div>
+					</div>
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel7.jpg" alt="">
+						</div>
+					</div>
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel8.jpg" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="dfboxcarousel dfnav3">
+				<h4 class="dfcarouselcap"><span>объединение с комнатой</span></h4>
+				<div class="dfcarousel3">
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel1.jpg" alt="">
+						</div>
+					</div>
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel2.jpg" alt="">
+						</div>
+					</div>
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel3.jpg" alt="">
+						</div>
+					</div>
+					<div class="item">
+						<div class="dfcarimg">
+							<img src="img/carousel4.jpg" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</section>	
 
 <?php require_once('includes/footer.php'); ?>
 
 </body>
 </html>
+
+
+<script>
+
+	//  var $owlitem1 = $('.dfcarousel1');
+ //     $owlitem1.trigger('destroy.owl.carousel');
+
+	// $owlitem1.html($owlitem2.find('.owl-stage-outer').html()).removeClass('owl-loaded');
+	// $owlitem1.owlCarousel({                           
+	// 	loop:true,
+	// 	nav:true, 
+	// 	autoplay:false,
+	// 	smartSpeed:1000,
+	// 	margin:15,    
+	// 	navText:['<span class="df-left"></span>','<span class="df-right"></span>'],
+	// 	responsive:{
+	// 		0:{
+	// 			items:1
+	// 		},
+	// 		410:{
+	// 			items: 2   
+	// 		},        
+	// 		700:{
+	// 			items:3
+	// 		},
+	// 		1000:{
+	// 			items:4
+	// 		}
+	// 	}
+
+	// });   
+
+
+
+	var owl1 = $(".dfcarousel1");
+	owl1.owlCarousel({
+		loop:true,
+		nav:true, 
+		autoplay:false,
+		smartSpeed:1000,
+		margin:25,
+		mouseDrag:false,
+		touchDrag: false,
+        center:false,     //если нужны обрезаные края
+        navText:['<span class="df-left"></span>','<span class="df-right"></span>'],
+        responsive:{
+        0:{
+        	items:1
+        },
+        410:{
+        	items: 2   
+        },        
+        900:{
+        	items:3
+        },
+        1200:{
+        	items:4
+        }
+        }
+    });
+
+    var owl2 = $(".dfcarousel2");
+	owl2.owlCarousel({
+		loop:true,
+		nav:true, 
+		autoplay:false,
+		smartSpeed:1000,
+		margin:25,
+		mouseDrag:false,
+		touchDrag: false,
+        center:false,     //если нужны обрезаные края
+        navText:['<span class="df-left"></span>','<span class="df-right"></span>'],
+        responsive:{
+        0:{
+        	items:1
+        },
+        410:{
+        	items: 2   
+        },        
+        900:{
+        	items:3
+        },
+        1200:{
+        	items:4
+        }
+        }
+    });
+
+    var owl3 = $(".dfcarousel3");
+	owl3.owlCarousel({
+		loop:true,
+		nav:true, 
+		autoplay:false,
+		smartSpeed:1000,
+		margin:25,
+		mouseDrag:false,
+		touchDrag: false,
+        center:false,     //если нужны обрезаные края
+        navText:['<span class="df-left"></span>','<span class="df-right"></span>'],
+        responsive:{
+        0:{
+        	items:1
+        },
+        410:{
+        	items: 2   
+        },        
+        900:{
+        	items:3
+        },
+        1200:{
+        	items:4
+        }
+        }
+    });
+
+
+</script>
